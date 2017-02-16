b63e30a5212e4e7a3f97345d39f53ff9eb35266b
Mc <694449212@qq.com>
2017-02-16 21:47:06 +0800

    init

---
+++ buzzfeed.py
@@ -0,0 +1,33 @@
+#coding:utf-8
+from datetime import datetime
+import requests
+import json
+import time
+import os
+startPage = '/videos?render_template=0&page=0'
+print('开始爬取Buzzfeed视频，每隔20分钟刷新一次。。。')
+def requestBuzzfeed(page):
+    print datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+    s = requests.get('https://www.buzzfeed.com'+page)
+    print(s.status_code)
+    result = json.loads(s.text)
+    cards = result["cards"]
+    nextPage = result["nextPage"]
+    if nextPage is None:
+        print('Video Over,刷新中... 等待20分钟')
+        # time.sleep(1*60*20)
+        # requestBuzzfeed(startPage)
+    else:
+        if cards == []:
+            print('card is null!')
+        else:
+            print("write file.")
+            f = open('videos/' + datetime.now().strftime('%Y-%m-%d-%H-%M-%S')+'.json','w')
+            f.write(json.dumps(cards))
+            f.close()
+        requestBuzzfeed(nextPage)
+if os.path.exists('videos'):
+    print('视频JSON文件保存在videos文件夹中...')
+else:
+    os.makedirs(r'videos')
+requestBuzzfeed(startPage)
